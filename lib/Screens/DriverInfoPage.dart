bcbfb5b99d2dd095e44998db2079cd14dc533d3b
jmn0 <jomannh16@gmail.com>
2025-03-19 03:11:35 +0300

    driver info temp commit

--- lib/Screens/DriverInfoPage.dart
+++ lib/Screens/DriverInfoPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'services.dart';
 
 class DriverInfoPage extends StatefulWidget {
   const DriverInfoPage({super.key});
