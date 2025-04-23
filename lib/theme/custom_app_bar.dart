347aedfe47b6f5f084287f761ef4063584a3b393
Maram-Almutairi <maram.f.d.almutairi@gmail.com>
2025-04-24 02:04:17 +0300

    اضافة صفحات مطلوبة ل الثيم

---
+++ lib/theme/custom_app_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:rem_s_appliceation9/core/utils/image_constant.dart';
+import 'package:rem_s_appliceation9/core/utils/size_utils.dart';
+import 'package:rem_s_appliceation9/theme/theme_helper.dart';
+import 'package:rem_s_appliceation9/widgets/custom_image_view.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showBackButton;
+
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+    this.showBackButton = true,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(60.h);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      decoration: BoxDecoration(
+        color: theme.colorScheme.onPrimaryContainer,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          if (showBackButton)
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+          Expanded(
+            child: Center(
+              child: CustomImageView(
+                imagePath: ImageConstant.img5935976241859510486,
+                height: 40.h,
+                width: 60.h,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
