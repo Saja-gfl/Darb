ac69b9cb774e6b08b8f3ec5deec022c991bab009
Maram-Almutairi <maram.f.d.almutairi@gmail.com>
2025-04-24 02:18:19 +0300

    ملف

---
+++ lib/widgets/custom_search_view.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:rem_s_appliceation9/core/utils/size_utils.dart';
+import 'package:rem_s_appliceation9/theme/custom_text_style.dart';
+import 'package:rem_s_appliceation9/theme/theme_helper.dart';
+import 'package:rem_s_appliceation9/widgets/custom_image_view.dart';
+
+class CustomSearchView extends StatelessWidget {
+  CustomSearchView({
+    Key? key,
+    this.alignment,
+    this.width,
+    this.scrollPadding,
+    this.controller,
+    this.focusNode,
+    this.autofocus = false,
+    this.textStyle,
+    this.textInputType = TextInputType.text,
+    this.maxLines,
+    this.hintText,
+    this.hintStyle,
+    this.prefix,
+    this.prefixConstraints,
+    this.suffix,
+    this.suffixConstraints,
+    this.contentPadding,
+    this.borderDecoration,
+    this.fillColor,
+    this.filled = true,
+    this.validator,
+    this.onChanged,
+    this.boxDecoration,
+  }) : super(key: key);
+
+  final AlignmentGeometry? alignment;
+  final double? width;
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final bool autofocus;
+  final TextStyle? textStyle;
+  final TextInputType textInputType;
+  final int? maxLines;
+  final String? hintText;
+  final TextStyle? hintStyle;
+  final Widget? prefix;
+  final BoxConstraints? prefixConstraints;
+  final Widget? suffix;
+  final BoxConstraints? suffixConstraints;
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? borderDecoration;
+  final Color? fillColor;
+  final bool filled;
+  final FormFieldValidator<String>? validator;
+  final Function(String)? onChanged;
+  final BoxDecoration? boxDecoration;
+  final EdgeInsets? scrollPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width ?? double.maxFinite,
+      decoration: boxDecoration,
+      child: TextFormField(
+        controller: controller,
+        focusNode: focusNode,
+        autofocus: autofocus,
+        style: textStyle ?? CustomTextStyles.bodyLargeBluegray200,
+        keyboardType: textInputType,
+        maxLines: maxLines ?? 1,
+        decoration: decoration,
+        validator: validator,
+        onChanged: onChanged,
+        onTapOutside: (event) {
+          if (focusNode != null) {
+            focusNode?.unfocus();
+          } else {
+            FocusManager.instance.primaryFocus?.unfocus();
+          }
+        },
+      ),
+    );
+  }
+
+  InputDecoration get decoration => InputDecoration(
+        hintText: hintText ?? "",
+        hintStyle: hintStyle ?? CustomTextStyles.bodyLargeBluegray200,
+        prefixIcon: prefix ??
+            Container(
+              margin: EdgeInsetsDirectional.fromSTEB(12.h, 14.h, 16.h, 14.h),
+              child: CustomImageView(
+                // imagePath: ImageConstant.imgSearch,
+                height: 16.h,
+                width: 16.h,
+              ),
+            ),
+        prefixIconConstraints: prefixConstraints ??
+            BoxConstraints(
+              maxHeight: 50.h,
+            ),
+        suffixIcon: suffix ??
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: 15.h,
+              ),
+              child: IconButton(
+                onPressed: () => controller!.clear(),
+                icon: Icon(
+                  Icons.clear,
+                  color: Colors.grey.shade600,
+                ),
+              ),
+            ),
+        suffixIconConstraints: suffixConstraints ??
+            BoxConstraints(
+              maxHeight: 50.h,
+            ),
+        isDense: true,
+        contentPadding: contentPadding ?? EdgeInsetsDirectional.all(12.h),
+        fillColor: fillColor ?? theme.colorScheme.onPrimaryContainer,
+        filled: filled,
+        border: borderDecoration ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.h),
+              borderSide: BorderSide(
+                color: theme.colorScheme.primary,
+                width: 1,
+              ),
+            ),
+        enabledBorder: borderDecoration ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.h),
+              borderSide: BorderSide(
+                color: theme.colorScheme.primary,
+                width: 1,
+              ),
+            ),
+        focusedBorder: (borderDecoration ??
+                OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.h),
+                ))
+            .copyWith(
+          borderSide: BorderSide(
+            color: theme.colorScheme.primary,
+            width: 1,
+          ),
+        ),
+      );
+}
